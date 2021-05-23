68ea70215a73d55586e1bba1dc65d8a634295d47
Essi Viippola <essi.viippola@live.fi>
2021-05-23 10:17:55 +0300

    Parse user input in JS

--- utils.js
+++ utils.js
@@ -2,6 +2,26 @@ let form = document.querySelector("#myform");
 
 form.addEventListener("change", function(e){
     console.log("Something changed!")
+
+    let selectedCar;
+    let selectedDistance;
+    let selectedSpeed1;
+    let selectedSpeed2;
+
+    const rbs = document.querySelectorAll("input[name='car-type'")
+    for(const rb of rbs){
+        if(rb.checked){
+            selectedCar = rb.value;
+            break;
+        }
+    }
+
+    selectedDistance = document.querySelector("#distance").value;
+    selectedSpeed1 = document.querySelector("#speed1").value;
+    selectedSpeed2 = document.querySelector("#speed2").value;
+    
+    console.log("Selected values: ", [selectedCar, selectedDistance, selectedSpeed1, selectedSpeed2]);
+
 });
 
 window.addEventListener("orientationchange", function(e){
