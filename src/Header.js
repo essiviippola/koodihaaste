42ad2d5ecaeb1b551bbcf5cc65f012df240fdbbe
samimaat <saminiksi@gmail.com>
2021-05-25 20:45:10 +0300

    Improved header CSS handling

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header(){
     return(
-        <header class="page-header header container-fluid">
+        <header class="page-header header">
             <div class="jumbotron">
                 <h1>Autoilumittarisovellus</h1>
                 <p>Autoilumittarin avulla voit verrata kahden eri nopeuden vaikutusta ajoneuvon kulutukseen.</p>
