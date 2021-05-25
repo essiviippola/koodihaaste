35ab91957a297f8f870677b49e7b6fea242878c0
Essi Viippola <essi.viippola@live.fi>
2021-05-25 20:11:05 +0300

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./App.css";
+import Footer from "./Footer"
 
 function App(){
     return(
         <div className="App">
             <p>Moi Sami!</p>
+            <Footer></Footer>
         </div>
     );
 }
