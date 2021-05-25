ceda4876c21572180cd385286502d38c9d798ed9
samimaat <saminiksi@gmail.com>
2021-05-25 20:19:22 +0300

    Standardising imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
+
 import "./index.css"
-import Header from "./Header"
 import "./App.css";
+
+import Header from "./Header"
 import Footer from "./Footer"
 
 function App() {
