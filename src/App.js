67d51bbacdcf39413d6c41105b292c288080116e
Essi Viippola <essi.viippola@live.fi>
2021-05-25 20:23:03 +0300

    React test button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./index.css"
-import Header from "./Header"
 import "./App.css";
+import Header from "./Header"
 import Footer from "./Footer"
+import Content from "./Content"
 
 function App() {
     return(
         <div className="App">
             <Header></Header>
             <p>Moi Sami!</p>
+            <Content></Content>
             <Footer></Footer>
         </div>
     );
