19d58db99b20c17986353baf627bc15bf35b2343
samimaat <saminiksi@gmail.com>
2021-05-25 20:48:35 +0300

    Remove unnecessary classes and divs

--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,9 @@ import React from "react";
 
 function Header(){
     return(
-        <header class="page-header header">
-            <div class="jumbotron">
-                <h1>Autoilumittarisovellus</h1>
-                <p>Autoilumittarin avulla voit verrata kahden eri nopeuden vaikutusta ajoneuvon kulutukseen.</p>
-            </div>
+        <header class="jumbotron">
+            <h1>Autoilumittarisovellus</h1>
+            <p>Autoilumittarin avulla voit verrata kahden eri nopeuden vaikutusta ajoneuvon kulutukseen.</p>
         </header>
     );
 }
