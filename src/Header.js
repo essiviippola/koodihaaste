642be6f9d8804060fdc784f9dc7295d13fbf9ee3
samimaat <saminiksi@gmail.com>
2021-05-25 20:45:30 +0300

    Revert "Improved header CSS handling"
    This reverts commit 42ad2d5ecaeb1b551bbcf5cc65f012df240fdbbe.

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header(){
     return(
-        <header class="page-header header">
+        <header class="page-header header container-fluid">
             <div class="jumbotron">
                 <h1>Autoilumittarisovellus</h1>
                 <p>Autoilumittarin avulla voit verrata kahden eri nopeuden vaikutusta ajoneuvon kulutukseen.</p>
