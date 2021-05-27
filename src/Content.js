1973eb20b70e977ed4e048ac42cb4bd59e31f1e7
Essi Viippola <essi.viippola@live.fi>
2021-05-27 21:07:15 +0300

    Move content from index.html to Content.js

--- src/Content.js
+++ src/Content.js
@@ -1,41 +1,53 @@
 import React from "react";
 
-class NameForm extends React.Component {
-    constructor(props){
+class InputForm extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {value: ""};
-
+        this.state = { value: "" };
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event){
-        this.setState({value: event.target.value});
+    handleChange(event) {
+        this.setState({ value: event.target.value });
     }
 
-    handleSubmit(event){
-        alert("A name was submitted: " + this.state.value);
-    }
-
-    render(){
-        return(
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+    render() {
+        return (
+            <form>
+                <div onChange={this.state.value} class="form-check-inline">
+                    <input type="radio" class="form-check-input" value="A" name="car" /> A
+                    <input type="radio" class="form-check-input" value="B" name="car" /> B
+                    <input type="radio" class="form-check-input" value="C" name="car" /> C
+                </div>
+                <div onChange={this.state.value}>
+                    <label>
+                        Etäisyys:
+                        <input class="form-control" type="number" placeholder="km" id="distance" name="distance" min="0" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
+                <div onChange={this.state.value}>
+                    <label>
+                        Nopeus 1:
+                        <input class="form-control" type="number" placeholder="km" id="distance" name="distance" min="0" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
+                <div onChange={this.state.value}>
+                    <label>
+                        Nopeus 2:
+                        <input class="form-control" type="number" placeholder="km" id="distance" name="distance" min="0" value={this.state.value} onChange={this.handleChange} />
+                    </label>
+                </div>
             </form>
         );
     }
 }
 
-function Content(){
-    return(
+function Content() {
+    return (
         <div class="row">
             <div class="col-md-4 border-right">
                 <p>Eka sarake</p>
-                <NameForm></NameForm>
+                <InputForm></InputForm>
             </div>
             <div class="col-md-8">
                 <p>Toinen sarake</p>
