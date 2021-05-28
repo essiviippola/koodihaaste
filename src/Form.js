5b01b2231ffd67478f648c510007c3ca15757b2c
Essi Viippola <essi.viippola@live.fi>
2021-05-28 19:49:26 +0300

    Style form

--- src/Form.js
+++ src/Form.js
@@ -22,25 +22,37 @@ class Form extends React.Component {
             <form>
                 <div className="form-group" onChange={this.handleChange}>
 
-                    <div className="form-check-inline">
-                        <input type="radio" value="A" name="car" /> A
-                        <input type="radio" value="B" name="car" /> B
-                        <input type="radio" value="C" name="car" /> C
-                    </div><br/><br/>
-
                     <div>
-                        <label>Etäisyys:</label>
-                        <input type="number" placeholder="km" id="distance" name="distance" min="0" />
+                        <input type="radio" value="A" name="car" id="car-a" className="form-check-inline"/>
+                        <label htmlFor="car-a">Auto A</label>
+                        <input type="radio" value="B" name="car" id="car-b" className="form-check-inline"/> 
+                        <label htmlFor="car-b">Auto B</label>
+                        <input type="radio" value="C" name="car" id="car-c" className="form-check-inline"/>
+                        <label htmlFor="car-c">Auto C</label>
                     </div><br/>
 
                     <div>
-                        <label>Nopeus 1:</label>
-                        <input type="number" placeholder="km/h" id="speed1" name="speed1" min="0"/>
+                        <label htmlFor="distance">Etäisyys</label>
+                        <div className="input-group mb-3">
+                            <input type="number" className="form-control" id="distance" name="distance" min="0" />
+                            <span className="input-group-text">km</span>
+                        </div>
                     </div><br/>
 
                     <div>
-                        <label>Nopeus 2:</label>    
-                        <input type="number" placeholder="km/h" id="speed2" name="speed2" min="0"/>
+                        <label htmlFor="speed1">Nopeus 1</label>
+                        <div className="input-group mb-3">
+                            <input type="number" className="form-control" id="speed1" name="speed1" min="0"/>
+                            <span className="input-group-text">km/h</span>
+                        </div>
+                    </div><br/>
+
+                    <div>
+                        <label htmlFor="speed2">Nopeus 2</label>
+                        <div className="input-group mb-3">
+                        <input type="number" className="form-control" id="speed2" name="speed2" min="0"/>
+                        <span className="input-group-text">km/h</span>
+                        </div>
                     </div>
                     
                 </div>
