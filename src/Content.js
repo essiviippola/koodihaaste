518109c917db45e4adf8c433d0759597a8898dbb
Essi Viippola <essi.viippola@live.fi>
2021-05-28 22:27:14 +0300

    Style cards

--- src/Content.js
+++ src/Content.js
@@ -1,20 +1,28 @@
 import React from "react";
 
-class Content extends React.Component{
-    render(){
-        return(
-            <div>
-                <div className="card">
-                    <p>Car: {this.props.valueFromParent.car}</p>
+class Content extends React.Component {
+    render() {
+        return (
+            <div id="content" className="row">
+                <div className="col-md-4">
+                    <div className="card">
+                        <h2>Kulutus</h2>
+                        <p>Car: {this.props.valueFromParent.car}</p>
+                    </div>
+                    <div className="card">
+                        <h2>Päästöt</h2>
+                        <p>Distance: {this.props.valueFromParent.distance}</p>
+                    </div>
                 </div>
-                <div className="card">
-                    <p>Distance: {this.props.valueFromParent.distance}</p>
-                </div>
-                <div className="card">
-                    <p>Speed 1: {this.props.valueFromParent.speed1}</p>
-                </div>
-                <div className="card">
-                    <p>Speed2: {this.props.valueFromParent.speed2}</p>
+                <div className="col-md-4">
+                    <div className="card">
+                        <h2>Nopeus</h2>
+                        <p>Speed 1: {this.props.valueFromParent.speed1}</p>
+                    </div>
+                    <div className="card">
+                        <h2>Hinta</h2>
+                        <p>Speed2: {this.props.valueFromParent.speed2}</p>
+                    </div>
                 </div>
             </div>
         );
