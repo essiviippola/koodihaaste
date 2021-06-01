92ff2c41a5a148987347a5e320048c6e2965c356
samimaat <saminiksi@gmail.com>
2021-06-01 21:26:19 +0300

    Make car icon in header an option

--- src/Header.js
+++ src/Header.js
@@ -6,12 +6,12 @@ function Header() {
             <h1>Autoilumittarisovellus</h1>
             <p>Autoilumittarin avulla voit verrata kahden eri nopeuden vaikutusta ajoneuvon kulutukseen.</p>
 
-            <div className="car-icon-line">
+            {/* <div className="car-icon-line">
                 <div className="car-line"></div>
                 <div className="car-icon">
                     <i className="fas fa-car-side fa-2x"></i>
                 </div>
-            </div>
+            </div> */}
 
         </header >
     );
