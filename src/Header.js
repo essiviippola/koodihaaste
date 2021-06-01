744a30be5986d4c09b242e389a2f173e608393bc
samimaat <saminiksi@gmail.com>
2021-06-01 20:14:20 +0300

    Modify header

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header(){
     return(
-        <header className="jumbotron vertical-center bg-primary">
+        <header className="jumbotron vertical-center bg-success">
             <h1>Autoilumittarisovellus</h1>
             <p>Autoilumittarin avulla voit verrata kahden eri nopeuden vaikutusta ajoneuvon kulutukseen.</p>
         </header>
