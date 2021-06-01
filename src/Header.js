fbe4db51781241a08ba04227909791585ecc169e
samimaat <saminiksi@gmail.com>
2021-06-01 21:15:08 +0300

    Move car icon to header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-function Header(){
-    return(
+function Header() {
+    return (
         <header className="jumbotron vertical-center bg-success">
             <h1>Autoilumittarisovellus</h1>
             <p>Autoilumittarin avulla voit verrata kahden eri nopeuden vaikutusta ajoneuvon kulutukseen.</p>
-        </header>
+
+            <div className="car-icon-line">
+                <div className="car-line"></div>
+                <div className="car-icon">
+                    <i className="fas fa-car-side fa-2x"></i>
+                </div>
+            </div>
+
+        </header >
     );
 }
 
