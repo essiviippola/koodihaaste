220828c5fbcc0bc4a8f1de462166a3f9dd1cbd82
Essi Viippola <essi.viippola@live.fi>
2021-06-01 17:54:13 +0300

    Fix bootstrap grids

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {calculateConsumption, calculateConsumptionDifference, calculateTime, calculateTimeDifference} from "./Utils.js";
+import { calculateConsumption, calculateConsumptionDifference, calculateTime, calculateTimeDifference } from "./Utils.js";
 
 class Content extends React.Component {
 
@@ -9,36 +9,36 @@ class Content extends React.Component {
         const distance = this.props.valueFromParent.distance;
         const speed1 = this.props.valueFromParent.speed1;
         const speed2 = this.props.valueFromParent.speed2;
-        
+
         return (
-            <div id="content" className="row">
-                <div className="col-md-4">
-                    <div>
-                        <h2>Kulutus</h2>
-                        <p>Kulutus: {calculateConsumption(car, distance, speed1)}</p>
-                        <p>Vertailtava kulutus: {calculateConsumption(car, distance, speed2)}</p>
-                        <p>Ero kulutuksessa: {calculateConsumptionDifference(car, distance, speed1, speed2)}</p>
-                    </div>
-                    <div>
-                        <h2>Päästöt</h2>
-                        <p>Päästöt: </p>
-                        <p>Vertailtavat päästöt: </p>
-                        <p>Erotus päästöissä: </p>
-                    </div>
+            <div id="content">
+                <div className="row">
+                        <div className="col">
+                            <h2>Kulutus</h2>
+                            <p>Kulutus: {calculateConsumption(car, distance, speed1)}</p>
+                            <p>Vertailtava kulutus: {calculateConsumption(car, distance, speed2)}</p>
+                            <p>Ero kulutuksessa: {calculateConsumptionDifference(car, distance, speed1, speed2)}</p>
+                        </div>
+                        <div className="col">
+                            <h2>Päästöt</h2>
+                            <p>Päästöt: </p>
+                            <p>Vertailtavat päästöt: </p>
+                            <p>Erotus päästöissä: </p>
+                        </div>
                 </div>
-                <div className="col-md-4">
-                    <div>
-                        <h2>Nopeus</h2>
-                        <p>Nopeus: {calculateTime(distance, speed1)}</p>
-                        <p>Vertailtava nopeus: {calculateTime(distance, speed2)}</p>
-                        <p>Erotus nopeudessa: {calculateTimeDifference(distance, speed1, speed2)}</p>
-                    </div>
-                    <div>
-                        <h2>Hinta</h2>
-                        <p>Hinta: </p>
-                        <p>Vertailtava hinta: </p>
-                        <p>Erotus hinnoissa: </p>
-                    </div>
+                <div className="row">
+                        <div className="col">
+                            <h2>Nopeus</h2>
+                            <p>Nopeus: {calculateTime(distance, speed1)}</p>
+                            <p>Vertailtava nopeus: {calculateTime(distance, speed2)}</p>
+                            <p>Erotus nopeudessa: {calculateTimeDifference(distance, speed1, speed2)}</p>
+                        </div>
+                        <div className="col">
+                            <h2>Hinta</h2>
+                            <p>Hinta: </p>
+                            <p>Vertailtava hinta: </p>
+                            <p>Erotus hinnoissa: </p>
+                        </div>
                 </div>
             </div>
         );
