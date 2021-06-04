353b24e5fda91df4226c6284e29152ef2964b00a
Essi Viippola <essi.viippola@live.fi>
2021-06-04 20:06:14 +0300

    Increase font size of form labels

--- src/Form.js
+++ src/Form.js
@@ -26,24 +26,24 @@ class Form extends React.Component {
         return (
             <form>
                 <div className="form-group" onChange={this.handleChange}>
-                    <label htmlFor="radio-buttons">Millä autolla ajat?</label>
+                    <label className="form-check-label" htmlFor="radio-buttons">Millä autolla ajat?</label>
                     <div className="radio-buttons">
                         <div className="form-check form-check-inline">
                             <input type="radio" value="A" name="car" id="car-a" className="form-check-input" />
-                            <label className="form-check-label" htmlFor="car-a">Auto A</label>
+                            <label htmlFor="car-a">Auto A</label>
                         </div>
                         <div className="form-check form-check-inline">
                             <input type="radio" value="B" name="car" id="car-b" className="form-check-input" />
-                            <label className="form-check-label" htmlFor="car-b">Auto B</label>
+                            <label htmlFor="car-b">Auto B</label>
                         </div>
                         <div className="form-check form-check-inline">
                             <input type="radio" value="C" name="car" id="car-c" className="form-check-input" />
-                            <label className="form-check-label" htmlFor="car-c">Auto C</label>
+                            <label htmlFor="car-c">Auto C</label>
                         </div>
                     </div>
 
                     <div>
-                        <label htmlFor="distance">Kuinka pitkä matka mökille?</label>
+                        <label className="form-check-label" htmlFor="distance">Kuinka pitkä matka mökille?</label>
                         <div className="input-group mb-3">
                             <input type="number" className="form-control" id="distance" name="distance" min="0" />
                             <span className="input-group-text">km</span>
@@ -51,7 +51,7 @@ class Form extends React.Component {
                     </div>
 
                     <div>
-                        <label htmlFor="speed1">Millä nopeudella?</label>
+                        <label className="form-check-label" htmlFor="speed1">Millä nopeudella?</label>
                         <div className="input-group mb-3">
                             <input type="number" className="form-control" id="speed1" name="speed1" min="0" />
                             <span className="input-group-text">km/h</span>
@@ -59,7 +59,7 @@ class Form extends React.Component {
                     </div>
 
                     <div>
-                        <label htmlFor="speed2">Mihin nopeuteen haluat verrata?</label>
+                        <label className="form-check-label" htmlFor="speed2">Mihin nopeuteen haluat verrata?</label>
                         <div className="input-group mb-3">
                             <input type="number" className="form-control" id="speed2" name="speed2" min="0" />
                             <span className="input-group-text">km/h</span>
