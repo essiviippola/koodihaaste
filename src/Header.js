0e3c233dc7d6e1a845b8e61d24aa49ed918189ed
Essi Viippola <essi.viippola@live.fi>
2021-06-04 17:28:48 +0300

    Change title

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <header className="jumbotron vertical-center">
-            <h1>Autoilumittarisovellus</h1>
+            <h1>Autoilumittari</h1>
             <p>Autoilumittarin avulla voit verrata kahden eri nopeuden vaikutusta ajoneuvon kulutukseen matkallasi mökille.</p>
         </header>
     );
