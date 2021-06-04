123271a47ff696ce925314ba48a76433859764c9
Essi Viippola <essi.viippola@live.fi>
2021-06-04 19:03:55 +0300

    Simplify code

--- src/Content.js
+++ src/Content.js
@@ -10,20 +10,30 @@ class Content extends React.Component {
         const speed1 = this.props.valueFromParent.speed1;
         const speed2 = this.props.valueFromParent.speed2;
 
+        const consumption1 = calculateConsumption(car, distance, speed1);
+        const consumption2 = calculateConsumption(car, distance, speed2);
+        const consumptionDifference = calculateConsumptionDifference(car, distance, speed1, speed2)
+
+        const time1 = calculateTime(distance, speed1);
+        const time2 = calculateTime(distance, speed2);
+        const timeDifference = calculateTimeDifference(distance, speed1, speed2);
+
         return (
             <div id="content">
                 <div className="row">
                     <div className="col">
                         <h2><span class="icon-h2"><i className="fas fa-charging-station"></i></span> Kulutus</h2>
-                        <p>Kulutus: {calculateConsumption(car, distance, speed1)}</p>
-                        <p>Vertailtava kulutus: {calculateConsumption(car, distance, speed2)}</p>
-                        <p>Ero kulutuksessa: {calculateConsumptionDifference(car, distance, speed1, speed2)}</p>
+                        <p>Ero kulutuksessa:</p>
+                        <h3>{consumptionDifference}</h3>
+                        <p>Kulutus: {consumption1}</p>
+                        <p>Vertailtava kulutus: {consumption2}</p>
                     </div>
                     <div className="col">
                         <h2><span class="icon-h2"><i className="far fa-clock"></i></span> Aika</h2>
-                        <p>Aika: {calculateTime(distance, speed1)}</p>
-                        <p>Vertailtava aika: {calculateTime(distance, speed2)}</p>
-                        <p>Ero ajassa: {calculateTimeDifference(distance, speed1, speed2)}</p>
+                        <p>Ero ajassa:</p>
+                        <h3>{timeDifference}</h3>
+                        <p>Aika: {time1}</p>
+                        <p>Vertailtava aika: {time2}</p>
                     </div>
                 </div>
             </div>
