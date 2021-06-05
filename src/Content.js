e4b9d7215e23f05cf949f5c8a1661d7b184f85ac
Essi Viippola <essi.viippola@live.fi>
2021-06-05 11:28:47 +0300

    Display time and consumption only once if speeds are the same

--- src/Content.js
+++ src/Content.js
@@ -18,11 +18,13 @@ class Content extends React.Component {
         const time2 = calculateTime(distance, speed2);
         const timeDifference = calculateTimeDifference(distance, speed1, speed2);
 
+        const slowerSpeed = Math.min(speed1, speed2);
+        const fasterSpeed = Math.max(speed1, speed2);
+
         return (
             <div id="content">
                 {timeDifference && consumptionDifference ?
                     <div>
-                        <hr />
                         <h2>
                             Ajamalla {speed1} km/h sijaan {speed2} km/h,
                                 matkassa kestää
@@ -31,17 +33,16 @@ class Content extends React.Component {
                                 <span className="emphasize"> {consumptionDifference} </span>
                                 vähemmän.
                             </h2>
-                        <hr />
                     </div>
                     : null}
                 <div className="row">
                     <div className="col">
                         {time1 ? <p>Aika nopeudella {speed1} km/h: <strong>{time1}</strong></p> : null}
-                        {time2 ? <p>Aika nopeudella {speed2} km/h: <strong>{time2}</strong></p> : null}
+                        {time2 && (time1 != time2) ? <p>Aika nopeudella {speed2} km/h: <strong>{time2}</strong></p> : null}
                     </div>
                     <div className="col">
                         {consumption1 ? <p>Kulutus nopeudella {speed1} km/h: <strong>{consumption1}</strong></p> : null}
-                        {consumption2 ? <p>Kulutus nopeudella {speed2} km/h: <strong>{consumption2}</strong></p> : null}
+                        {consumption2 && (consumption1 != consumption2) ? <p>Kulutus nopeudella {speed2} km/h: <strong>{consumption2}</strong></p> : null}
                     </div>
                 </div>
 
