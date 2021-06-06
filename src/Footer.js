c319c5577475cbf1a175e00a5946c43b830612bd
samimaat <saminiksi@gmail.com>
2021-06-06 15:08:39 +0300

    Add description

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function Footer(){
             <div className="row">
             <div className="col-lg-8 col-md-8 col-sm-12">
                 <h6 className="text-uppercase">Lisätietoa</h6>
-                <p>LISÄÄ KUVAUS</p>
+                <p>Web-sovellus käyttää Solidabiksen kevään 2021 koodihaasteessa annettuja autojen kulutustietoja ja laskee käyttäjän antamien tietojen (matka mökille, nopeus ja vertailtava nopeus) perusteella kuinka paljon vähemmän aikaa matkassa kestää ja kuinka paljon kulutus kasvaa. Näiden päätietojen lisäksi sovellus antaa kummankin nopeuden polttoainekulutuksen, matka-ajan ja keskikulutuksen (per 100 kilometriä).</p>
             </div>
             <div className="col-lg-4 col-md-4 col-sm-12">
                 <h6 className="text-uppercase">Yhteystiedot</h6>
