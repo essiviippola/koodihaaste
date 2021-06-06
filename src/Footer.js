e672ca1e564f6d3b820febe7052aeb38088651ff
samimaat <saminiksi@gmail.com>
2021-06-06 15:11:56 +0300

    Modify h[x] structure and preserve styling

--- src/Footer.js
+++ src/Footer.js
@@ -5,11 +5,11 @@ function Footer(){
         <footer className="footer page-footer">
             <div className="row">
             <div className="col-lg-8 col-md-8 col-sm-12">
-                <h6 className="text-uppercase">Lisätietoa</h6>
+                <h2 className="h6 text-uppercase">Lisätietoa</h2>
                 <p>Web-sovellus käyttää Solidabiksen kevään 2021 koodihaasteessa annettuja autojen kulutustietoja ja laskee käyttäjän antamien tietojen (matka mökille, nopeus ja vertailtava nopeus) perusteella kuinka paljon vähemmän aikaa matkassa kestää ja kuinka paljon kulutus kasvaa. Näiden päätietojen lisäksi sovellus antaa kummankin nopeuden polttoainekulutuksen, matka-ajan ja keskikulutuksen (per 100 kilometriä).</p>
             </div>
             <div className="col-lg-4 col-md-4 col-sm-12">
-                <h6 className="text-uppercase">Yhteystiedot</h6>
+                <h2 className="h6 text-uppercase">Yhteystiedot</h2>
                 <p>Essi Viippola
                     <br/><i className="bi bi-envelope" role="img" aria-label="email"></i> essi.viippola@live.fi
                     <br/><i className="bi-github" role="img" aria-label="GitHub"></i> <a className="url" href="https://github.com/essiviippola" target="_blank">essiviippola</a>
