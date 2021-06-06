1df80c20c83dcc5a4b51efad97ac911993abb9ec
Essi Viippola <essi.viippola@live.fi>
2021-06-06 14:36:29 +0300

    Add contact information to footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,18 +6,17 @@ function Footer(){
             <div className="row">
             <div className="col-lg-8 col-md-8 col-sm-12">
                 <h6 className="text-uppercase">Lisätietoa</h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat
-                    ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+                <p>LISÄÄ KUVAUS</p>
             </div>
             <div className="col-lg-4 col-md-4 col-sm-12">
                 <h6 className="text-uppercase">Yhteystiedot</h6>
-                <p>Sami Määttä
-                    <br/><i className="bi bi-envelope" role="img" aria-label="email"></i> saomaatta@gmail.com
-                    <br/><i className="bi-github" role="img" aria-label="GitHub"></i> Samin GitHub
-                    <br/>
-                    <br/>Essi Viippola
+                <p>Essi Viippola
                     <br/><i className="bi bi-envelope" role="img" aria-label="email"></i> essi.viippola@live.fi
-                    <br/><i className="bi-github" role="img" aria-label="GitHub"></i> Essin GitHub
+                    <br/><i className="bi-github" role="img" aria-label="GitHub"></i> <a className="url" href="https://github.com/essiviippola" target="_blank">essiviippola</a>
+                    <br/><br/>Sami Määttä
+                    <br/><i className="bi bi-envelope" role="img" aria-label="email"></i> saomaatta@gmail.com
+                    <br/><i className="bi-github" role="img" aria-label="GitHub"></i> <a className="url" href="https://github.com/samimaat" target="_blank">samimaat</a>
+                    <br/>
                 </p>
             </div>
         </div>
